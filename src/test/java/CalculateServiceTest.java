22e485be4d49667641a51f9642c9614fbf667000
Lev <finvancan@gmail.com>
2024-10-24 11:36:33 +0300

    Edit CalculateService add money / 3, edit CalculateServiceTest;

--- src/test/java/CalculateServiceTest.java
+++ src/test/java/CalculateServiceTest.java
@@ -7,14 +7,11 @@ public class CalculateServiceTest {
     public void testCalculate() {
         CalculateService calculateService = new CalculateService();
 
-        int result1 = calculateService.calculate(10000, 3000);
-        Assertions.assertEquals(9, result1);
+        int result1 = calculateService.calculate(10000, 3000, 20000);
+        Assertions.assertEquals(3, result1);
 
-        int result2 = calculateService.calculate(100000, 60000);
-        Assertions.assertEquals(7, result2);
+        int result2 = calculateService.calculate(100000, 60000, 150000);
+        Assertions.assertEquals(2, result2);
 
-        // Другие тесты
-        int result3 = calculateService.calculate(15000, 4000);
-        Assertions.assertEquals(9, result3);
     }
 }
